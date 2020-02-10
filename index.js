0d39cbf49170bb8c0fb23ca04f41058f914af751
adamwinzdesign <adamwinzdesign@gmail.com>
2020-02-10 11:55:19 -0600

    adding endpoints

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+
+const express = require('express');
+
+const Users = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+// GET to '/'
+server.get('/', function(req, res) {
+  res.send({ hello: 'Hello from node-api1-project, in index.js!' });
+});
+
+// GET list of users
+server.get('api/users', (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+      console.log('res in hubs GET request: ', res)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: 'error getting list of users in index.js, server.get, Users.find' })
+    })
+})
+
+// GET user by id
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  Users.findById(id)
+    .then(user => {
+      res.status(200).json(user);
+      console.log(`findByID ${id} worked!`)
+    })
+})
+
