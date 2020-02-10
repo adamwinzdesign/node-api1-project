438903ff832cf5f06edef49d50c2462a6e0fb384
adamwinzdesign <adamwinzdesign@gmail.com>
2020-02-10 12:22:06 -0600

    still building

--- index.js
+++ index.js
@@ -35,5 +35,57 @@ server.get('/api/users/:id', (req, res) => {
       res.status(200).json(user);
       console.log(`findByID ${id} worked!`)
     })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: 'The user with the specified ID does not exist.' })
+    })
 })
 
+// create a new User with POST
+server.post('/api/users', (req, res) => {
+  const userData = req.body;
+  // validation will go here once we learn how
+
+  // still need to add logic if req.body is missing name or bio!
+
+  Users.insert(userData)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: 'There was an error while saving the user to the database' })
+    })
+})
+
+// delete a user
+server.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  Users.remove(id)
+    .then(removed => {
+      res.status(200).json(removed)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: 'Error removing user' })
+    })
+})
+
+// update user with PUT
+server.put('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  const user = req.body;
+
+  Users.update(id, user)
+    .then(updated => {
+      res.status(204).json(updated);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: 'Error updating the user' })
+    })
+});
+
+const port = 7000;
+server.listen(port, () => console.log(`** Server listening on port ${port} in node-api1-project! **`));
